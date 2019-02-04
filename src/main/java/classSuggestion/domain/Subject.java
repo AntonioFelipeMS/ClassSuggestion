6f8b02bc73beb27d5cef00a2c66949b6697cf5ad
Antonio Felipe <antonio.silva@m4u.com.br>
2019-02-04 11:51:01 -0200

    Inserting Subject entity and ddl

--- src/main/java/classSuggestion/domain/Subject.java
+++ src/main/java/classSuggestion/domain/Subject.java
@@ -1,4 +1,25 @@
 package classSuggestion.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Subject {
+
+    @Id
+    private Integer id;
+
+    @Column
+    private String description;
+
+    @Column
+    private Integer semester;
+
+    @Column
+    private String weight;
+
+    @Column
+    private boolean credit_lockable;
+
 }
